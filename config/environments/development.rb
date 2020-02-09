db01d105132ce414c09ba248700bd264ea8b69a4
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2020-02-09 13:16:38 +0100

    indentation cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
 # Default configuration for action_mailer with devise in dev env.
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
