7b4efdea44dfd75a653ab4a1e1588805069e3ee2
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2020-11-21 08:53:08 +0100

    disable user authentication on this branch for now as we are not storing any data - security is not implemented.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_action :authenticate_user!
+  # for now, disable authentication and authorization as this is not fully implemented yet.
+  # we don't need security yet as we are only converting tables to different formats and not storing any data.
+  
+  # before_action :authenticate_user!
 end
